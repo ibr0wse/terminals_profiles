6d6468bdaaee027c9ff2a9412b2439d4cfe0c0a5
ibr0wse <73321701+ibr0wse@users.noreply.github.com>
2021-02-24 09:18:44 -0500

    Create zsh_tmux.sh

---
+++ zsh_tmux.sh
@@ -0,0 +1,40 @@
+
+########INSTALLS,UPDATES,UPGRADES########
+sudo apt-get install tmux curl zsh git net-tools -y && sudo apt-get update -y && sudo apt-get upgrade -y
+
+########ZSH########
+### ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+### powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
+source ~/.zshrc
+### go through the setup^
+
+### zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+### zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+sed -i 's/(git)/(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+source ~/.zshrc
+
+########TMUX########
+### TMUX plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+cat > ~/.tmux.conf <<EOF
+# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+set -g @plugin 'tmux-plugins/tmux-logging'
+
+set -g history-limit 50000
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run '~/.tmux/plugins/tpm/tpm'
+EOF
+### Hop into a new tmux, prefix + I to update the new tmux plugin stuffs 
+# OR
+### If in a TMUX terminal already, RUN THIS TO REFRESH NEW PLUGINS:
+tmux source ~/.tmux.conf
+### then prefix + I
