38ba18bf0da587fc93b1b52df8e6f3182040a6ba
ibr0wse <73321701+ibr0wse@users.noreply.github.com>
2024-10-21 11:08:15 -0400

    Update zsh_tmux.sh

--- zsh_tmux.sh
+++ zsh_tmux.sh
@@ -25,13 +25,17 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cat > ~/.tmux.conf <<EOF
 # List of plugins
+# List of plugins
 set -g @plugin 'tmux-plugins/tpm'
 set -g @plugin 'tmux-plugins/tmux-sensible'
 set -g @plugin 'tmux-plugins/tmux-logging'
 set -g @logging-path "$HOME/logs/%Y-%m-%d"
-set -g @logging-filename '%Y-%m-%d_%H-%M-%S_#{session_name}-#{window_index}-#{pane_index}-#{window_name}.log'
+# Logging file name with date-time and window + pane information
+set -g @logging-filename '%Y-%m-%d_%H-%M-%S_#{session_name}-#{window_index}-#{pane_index}-#{pane_title}.log'
 set -g history-limit 50000
 set -g mouse on
+# Bind a shortcut to rename the current pane
+bind-key r command-prompt "select-pane -T '%%'"
 # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
 run '~/.tmux/plugins/tpm/tpm'
 EOF
